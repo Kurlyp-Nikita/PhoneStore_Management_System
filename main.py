68af5f24c9dbde9d628681aebcaab05c7e6076e8
Kurlyp-Nikita <ilinnikita244@gmail.com>
2026-02-11 00:37:57 +0300

    fix code

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from collections.abc import MutableSet
 
 
 class Phone:
-    def __init__(self, barnd: str, system: str, price: int, color: str):
+    def __init__(self, barnd: str, name: str, price: int, color: str):
         self.brand = barnd
-        self.system = system
+        self.name = name
         self.price = price
         self.color = color
         self.phone_store = None
 
     def __str__(self):
         return f"Брэнд: {self.brand}, \n" \
-               f"ОС: {self.system}, \n" \
+               f"Марка: {self.name}, \n" \
                f"Цена: {self.price}$, \n" \
                f"Цвет: {self.color}"
 
@@ -23,36 +23,23 @@ class PhoneStore:
 
     def __str__(self):
         return f"Имя магазина: {self.name} \n" \
-               f"Телефон: \n\n" \
+               f"Телефоны: \n\n" \
                f"{self.phone} \n\n"
 
 
 class PhoneSet(MutableSet[Phone]):
     def __init__(self, phone_store: PhoneStore):
-        if not isinstance(phone_store, PhoneStore):
-            raise TypeError("Указан не магазин телефонов!")
-
-        if phone_store.phone != None:
-            raise NotImplemented("Для указанного магазина уже существует салон!")
-
         phone_store.phone = self
         self.elements = set()
 
     def add(self, phone: Phone):
-        if not isinstance(phone, Phone):
-            raise TypeError("Добавляемый объект должен быть телефоном!")
-
-        if phone.phone_store != None:
-            raise ValueError(f"{phone} находится в другом магазине.")
-
-        if not phone in self.elements:
-            self.elements.add(phone)
+        self.elements.add(phone)
 
     def discard(self, phone: Phone):
         self.elements.remove(phone)
 
     def __str__(self):
-        return " ".join(str(i) for i in self.elements)
+        return "\n\n".join(str(i) for i in self.elements)
 
     def __iter__(self):
         return iter(self.elements)
@@ -64,26 +51,39 @@ class PhoneSet(MutableSet[Phone]):
         return len(self.elements)
 
 
-phone_1 = Phone("Samsung", "Android", 1000, "red")
-phone_2 = Phone("Aple", "Iphone", 1200, "black")
-print(phone_1)
+phone_samsung_1 = Phone("Samsung", "Samsung A55", 1500, "red")
+phone_samsung_2 = Phone("Samsung", "Samsung A52", 1200, "green")
+phone_samsung_3 = Phone("Samsung", "Samsung A50", 1000, "black")
 
-print("-" * 200)
+phone_apple_1 = Phone("Apple", "Iphone 5", 1200, "black")
+phone_apple_2 = Phone("Apple", "Iphone 6", 1500, "gold")
+phone_apple_3 = Phone("Apple", "Iphone 10", 2000, "yellow")
+
 
 phone_store_1 = PhoneStore("Samsung-Store")
-phone_set_1 = PhoneSet(phone_store_1)
-
-
 phone_store_2 = PhoneStore("Apple-Store")
-phone_set_2 = PhoneSet(phone_store_2)
 
-
-phone_set_1.add(phone_1)
+phone_set_1 = PhoneSet(phone_store_1)
+phone_set_1.add(phone_samsung_1)
+phone_set_1.add(phone_samsung_2)
+phone_set_1.add(phone_samsung_3)
+phone_set_1.discard(phone_samsung_3)
 print(phone_store_1)
+print(len(phone_set_1))
+print("-" * 200)
 
 
-phone_set_2.add(phone_2)
+phone_set_2 = PhoneSet(phone_store_2)
+phone_set_2.add(phone_apple_1)
+phone_set_2.add(phone_apple_2)
+phone_set_2.add(phone_apple_3)
+phone_set_2.discard(phone_apple_3)
+
+
 print(phone_store_2)
+print(len(phone_set_2))
 
-
+# it = iter(phone_set_2)
+# print(next(it))
+# print(next(it))
 
