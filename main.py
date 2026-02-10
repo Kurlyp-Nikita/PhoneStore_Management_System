c53294c8c94a003b20fbfc3f1ce4de4c312e3716
Kurlyp-Nikita <ilinnikita244@gmail.com>
2026-02-10 14:41:24 +0300

    create class Phone

---
+++ main.py
@@ -0,0 +1,11 @@
+from collections.abc import MutableSet
+
+
+class Phone:
+    def __init__(self, brand: str, price: int, color: str):
+        self.brand = brand
+        self.price = price
+        self.color = color
+
+
+
