96439987cd60c082981fa61070f79ddada161f6a
Kurlyp-Nikita <ilinnikita244@gmail.com>
2026-02-10 15:37:03 +0300

    add dander methods

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ class Phone:
         self.price = price
         self.color = color
 
+    def __str__(self):
+        return f"Марка: {self.brand}, Цена: {self.price}$, Цвет: {self.color}"
+
 
 class PhoneStore:
     def __init__(self, name: str):
         self.name = name
         self.store: PhoneSet = None
+
+    def __str__(self):
+        return f"Имя магазина: {self.name}, телефон: {self.store}"
 
 
 class PhoneSet(MutableSet[Phone]):
@@ -24,4 +30,33 @@ class PhoneSet(MutableSet[Phone]):
 
     def discard(self, phone: Phone):
         self.elements.remove(phone)
+
+    def __str__(self):
+        return f"{self.elements}"
+
+    def __iter__(self):
+        return iter(self.elements)
+
+    def __contains__(self, item):
+        return item in self.elements
+
+    def __len__(self):
+        return len(self.elements)
+
+
+phone_1 = Phone("Android", 1000, "red")
+print(phone_1)
+
+print("-" * 200)
+
+phone_store_1 = PhoneStore("Samsung-Store")
+print(phone_store_1)
+
+print("-" * 200)
+
+phone_set_1 = PhoneSet(phone_store_1)
+print(phone_set_1)
+
+
+
 
